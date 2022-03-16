b7a7d027f13bf3ee2088d451b65941e8e9e7ed9b
miguelherrerad <57775873+miguelherrerad@users.noreply.github.com>
2022-03-16 12:32:57 -0500

    Double Bird

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Email And PassWord Login',
+      title: 'Ingreso',
       theme: ThemeData(
         // This is the theme of your application.
         //
