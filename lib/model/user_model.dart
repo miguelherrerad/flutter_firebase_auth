b41716b4b12d67aba846cde07c45aaae9e6b38e8
miguelherrerad <57775873+miguelherrerad@users.noreply.github.com>
2022-03-17 14:25:51 -0500

    sending data from server

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -6,7 +6,7 @@ class UserModel {
 
   UserModel({this.uid, this.email, this.firstName, this.secondName});
 
-  //data from server
+  //receiving data from server
   factory UserModel.fromMap(map) {
     return UserModel(
       uid: map['uid'],
@@ -14,5 +14,14 @@ class UserModel {
       firstName: map['firstName'],
       secondName: map['secondName'],
     );
+  }
+  //sending data to our server
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'email': email,
+      'firstName': firstName,
+      'secondName': secondName,
+    };
   }
 }
